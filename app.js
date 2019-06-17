ec9802d2cceea734299ad91b530c7190c3101d27
M03G <mayankgupta.mg962@gmail.com>
2019-06-17 07:27:15 +0530

    Second Commit Level 4 security

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs =  require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require("md5");
+const saltRounds = 10;
 const app = express();
 
 app.use(express.static("public"));
@@ -20,8 +23,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password']});
+// for encryption
+// userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("user", userSchema);
 
@@ -38,17 +41,21 @@ app.get("/register",function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email :req.body.username,
-    password: req.body.password
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email :req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  });
+
 });
 
 
@@ -62,9 +69,13 @@ app.post("/login", function(req, res){
         console.log(err);
       }else{
         if(foundUser){
-          if(foundUser.password === password){
-            res.render("secrets");
-          }
+
+          bcrypt.compare(password, foundUser.password, function(err, result){
+            if(result=== true){
+              res.render("secrets");
+            }
+          });
+
         }
       }
     });
